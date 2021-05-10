afb03361d055d676ab5a4a3e007931ea63d1a6ae
langnerc <c.langner@mailbox.org>
2021-05-10 17:26:45 +0200

    Change return value

--- src/main/java/de/thbrandenburg/nitze/webapp/WebAppApplication.java
+++ src/main/java/de/thbrandenburg/nitze/webapp/WebAppApplication.java
@@ -15,6 +15,6 @@ public class WebAppApplication {
 	}
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+		return String.format("Hallo %s! Schön, dass du da bist!", name);
 	}
 }
